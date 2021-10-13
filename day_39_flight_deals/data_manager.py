a18007d9a369a9706727c13d16de1ead99aeb3d2
Brian Mendes <btwmendes@gmail.com>
2021-10-12 18:45:33 -0700

    tuesdays progress

--- day_39_flight_deals/data_manager.py
+++ day_39_flight_deals/data_manager.py
@@ -1,3 +1,32 @@
-class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+import requests
+from pprint import pprint
+
+sheet_endpoint = "https://api.sheety.co/08e59ed6f95edc2c406f133d0a7bc7e4/flightDeals/prices"
+sheet_header = {
+    "Authorization": "Basic YnR3bWVuZGVzOlNwcmluZ2ZpZWxkMTch"
+}
+
+city = "Los Angeles"
+airport_code = "lax"
+price = 205.03
+
+# for flight in flights:
+sheet_inputs = {
+    "price": {
+        "city": city,
+        "iatacodes": airport_code,
+        "lowestprice": price
+    }
+}
+
+# response = requests.get(url=sheet_endpoint, headers=sheet_header)
+# data = response.json()
+# pprint(data)
+
+sheet_response = requests.post(url=sheet_endpoint, json=sheet_inputs, headers=sheet_header)
+print(sheet_response.text)
+
+
+# class DataManager:
+#     #This class is responsible for talking to the Google Sheet.
+#     pass
--- day_39_flight_deals/flight_data.py
+++ day_39_flight_deals/flight_data.py
@@ -1,3 +1,18 @@
+import requests
+
+flight_endpoint = ""
+
+flight_params = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+headers = {
+    
+}
 class FlightData:
     #This class is responsible for structuring the flight data.
     pass
